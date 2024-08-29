46fe54453b9ee8a99c0b6a453b02a4a9d8a06795
windowslee <slee12463@gmail.com>
2024-08-29 18:16:43 -0400

    be happy

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,11 @@
-import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 import { PlusSquareIcon } from "@chakra-ui/icons";
@@ -6,42 +13,54 @@ import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
-	const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
-	return (
-		<Container maxW={"1140px"} px={4}>
-			<Flex
-				h={16}
-				alignItems={"center"}
-				justifyContent={"space-between"}
-				flexDir={{
-					base: "column",
-					sm: "row",
-				}}
-			>
-				<Text
-					fontSize={{ base: "22", sm: "28" }}
-					fontWeight={"bold"}
-					textTransform={"uppercase"}
-					textAlign={"center"}
-					bgGradient={"linear(to-r, cyan.400, blue.500)"}
-					bgClip={"text"}
-				>
-					<Link to={"/"}>Nerd Store</Link>
-				</Text>
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "30", sm: "30" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          
+        >
+		
+          <Link to={"/"}>🤓</Link>
+        </Text>
 
-				<HStack spacing={2} alignItems={"center"}>
-					<Link to={"/create"}>
-						<Button>
-							<PlusSquareIcon fontSize={20} />
-						</Button>
-					</Link>
-					<Button onClick={toggleColorMode}>
-						{colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
-					</Button>
-				</HStack>
-			</Flex>
-		</Container>
-	);
+		<Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+		
+          <Link to={"/"}>Nerd Store</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 };
 export default Navbar;

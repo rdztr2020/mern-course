dbaca7a09a1aaa45b12385c397941e5fc4416025
windowslee <slee12463@gmail.com>
2024-09-02 17:06:42 -0400

    again

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -20,6 +20,6 @@ const productSchema = new mongoose.Schema(
 	}
 );
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model("nerdstore", productSchema);
 
 export default Product;
